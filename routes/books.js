052c6ca8852915038bc227bc092ce6def84ff24a
elizhang227 <zhang.eli@hotmail.com>
2019-06-07 10:06:50 -0400

    added routes and models for books

---
+++ routes/books.js
@@ -0,0 +1,25 @@
+const express = require('express'),
+    router = express.Router(),
+    booksModel = require('../models/books');
+
+router.get('/', async function(req, res, next) {
+    const allBooks = await booksModel.getAllBooks();
+
+    if(!!req.session.is_logged_in) {
+        res.render('template', {
+            locals: {
+                title: 'Home page',
+                booksList: allBooks,
+                is_logged_in: req.session.is_logged_in,
+                userName: req.session.first_name
+            },
+            partials: {
+                content: 'partial-restaurants'
+            }
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
+module.exports = router;
