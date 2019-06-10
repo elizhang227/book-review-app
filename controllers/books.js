c4e79ae05faaa8b638aadfffac0ba0720a1099a1
elizhang227 <zhang.eli@hotmail.com>
2019-06-10 11:35:34 -0400

    added controller for post route for submitting reviews

--- controllers/books.js
+++ controllers/books.js
@@ -41,3 +41,39 @@ exports.book_id_get = async (req, res) => {
         }
     });
 }
+
+exports.book_id_post = async (req, res) => {
+    console.log("this is the req body", req.body);
+    const { review } = req.body;
+    console.log("this is the req params", req.params);
+    const bookID = req.params.book_id;
+    const bookInfo = await booksModel.getBookInfo(bookID);
+    const userID = await booksModel.getUser(req.session.email);
+
+    if(!!req.session.is_logged_in) {
+        booksModel.addReview(review, bookID, userID.id)
+        .then(async () => {
+            res.redirect(`/books/${bookID}`); // post-get-redirect to avoid form resubmission
+            const allReviews = await booksModel.getAllReviewsForBook(bookID);
+
+            res.status(200).render('template', {
+                locals: {
+                    title: 'List of Reviews for the book: ',
+                    is_logged_in: req.session.is_logged_in,
+                    reviewsList: allReviews,
+                    userName: req.session.first_name,
+                    bookInfo: bookInfo,
+                    email: req.session.email
+                },
+                partials: {
+                    content: 'partial-reviews'
+                }
+            });
+        })
+        .catch((err) => {
+            res.sendStatus(500).send(err.message);
+        });
+    } else {
+        res.redirect('/');
+    }
+}
--- routes/books.js
+++ routes/books.js
@@ -7,41 +7,6 @@ router.get('/', booksController.homepage_get);
 
 router.get('/:book_id', booksController.book_id_get);
 
-router.post('/:book_id', async function(req, res, next) {
-    console.log("this is the req body", req.body);
-    const { review } = req.body;
-    console.log("this is the req params", req.params);
-    const bookID = req.params.book_id;
-    const bookInfo = await booksModel.getBookInfo(bookID);
-    const userID = await booksModel.getUser(req.session.email);
-
-    if(!!req.session.is_logged_in) {
-        booksModel.addReview(review, bookID, userID.id)
-        .then(async () => {
-            res.redirect(`/books/${bookID}`); // post-get-redirect to avoid form resubmission
-            const allReviews = await booksModel.getAllReviewsForBook(bookID);
-
-            res.status(200).render('template', {
-                locals: {
-                    title: 'List of Reviews for the book: ',
-                    is_logged_in: req.session.is_logged_in,
-                    reviewsList: allReviews,
-                    userName: req.session.first_name,
-                    bookInfo: bookInfo,
-                    email: req.session.email
-                },
-                partials: {
-                    content: 'partial-reviews'
-                }
-            });
-
-        })
-        .catch((err) => {
-            res.sendStatus(500).send(err.message);
-        });
-    } else {
-        res.redirect('/');
-    }
-});
+router.post('/:book_id', booksController.book_id_post);
 
 module.exports = router;
