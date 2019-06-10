c7f38e47b5d9d5c617f3d5a32e43e500621d5dfa
elizhang227 <zhang.eli@hotmail.com>
2019-06-10 11:39:19 -0400

    removed unncessary lines

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,9 @@
 const express = require('express'),
     router = express.Router(),
-    booksController = require('../controllers/books'),
-    booksModel = require('../models/books');
+    booksController = require('../controllers/books');
 
 router.get('/', booksController.homepage_get);
-
 router.get('/:book_id', booksController.book_id_get);
-
 router.post('/:book_id', booksController.book_id_post);
 
 module.exports = router;
